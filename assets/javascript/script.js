9d24360ab619b5ce6bb877f365fd746bf3065cd7
mecheresvalentin <gabriel.v.mecheres@gmail.com>
2021-05-23 01:06:54 +0300

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,29 @@
+var btn = document.getElementById('addC');
+btn.onclick = Counter;
+var clicks = document.querySelector('.clicks');
+var a = 1;
+function Counter() {
+  a += 1;
+  clicks.innerHTML = a;
+}
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
